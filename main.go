b1d4ca49ba1c5d00d7fc9289346e680ae94e54fc
nanzi <13121199731@163.com>
2022-02-25 00:54:42 +0800

    add some

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Data struct {
@@ -19,13 +21,25 @@ func index(w http.ResponseWriter, i *http.Request) {
 	w.Write(jstr)
 }
 
+func indexHtml(w http.ResponseWriter, i *http.Request) {
+	var data Data
+	data.Title = "我的标题"
+	data.Body = "我的文本"
+	t := template.New("index.html")
+	path, _ := os.Getwd()
+	t, _ = t.ParseFiles(path + "/template/index.html")
+	t.Execute(w, data)
+}
+
 func main() {
 	//程序入口
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
 
+	//路由
 	http.HandleFunc("/", index)
+	http.HandleFunc("/index.html", indexHtml)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Println(err)
